6acd00600e07f2e4614556f7f1d00c3ce3db9d19
metralha61 <83584897+metralha61@users.noreply.github.com>
2024-10-10 10:57:31 -0300

    Calculo de Media
    Esse codigo faz o calculo das 3 notas do aluno, aonde no final do processo faz o media, e determina se passou ou não de ano.

---
+++ main.py
@@ -0,0 +1,12 @@
+nota1=float(input("digite sua primeira nota:"))
+nota2=float(input("digite sua segunda nota:"))
+nota3=float(input("digite sua terceira nota:"))
+
+media = (nota1+nota2+nota3) /3 
+
+if (media>=6):
+    print("parabens voce passou de ano, sua media foi:",media)
+else:
+    print("infelizmente voce reprovou de ano, sua media foi:", media)
+
+
